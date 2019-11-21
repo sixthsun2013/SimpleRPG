a6f4f2989068581411bac97a7286e2fc8b93f837
Sixth Sun <sixthsunltd@gmail.com>
2019-11-21 09:17:54 +0000

    Added Item class model

---
+++ Engine/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Item
+    {
+        public int ID { get; set; }
+
+        public int Name { get; set; }
+
+        public int NamePlural { get; set; }
+    }
+}
