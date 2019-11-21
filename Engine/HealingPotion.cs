8e822172b528d609b7d08a2be798dc819e3522d3
Sixth Sun <sixthsunltd@gmail.com>
2019-11-21 09:16:29 +0000

    Created healping potion class model

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class HealingPotion
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string NamePlural { get; set; }
+
+        public int AmountToHeal { get; set; }
+    }
+}
