34bf13288448be1cc4f83d4d24067b00221918ad
Sixth Sun <sixthsunltd@gmail.com>
2019-11-21 09:19:30 +0000

    Added Location class model

---
+++ Engine/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class Location
+    {
+        public int ID { get; set; }
+
+        public int Name { get; set; }
+
+        public int Description { get; set; }
+    }
+}
